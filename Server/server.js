4137c931c754b254f1aff240b0ccb76d5a09b818
Dheerendra Vikram Dixit <dheerendradixit321@gmail.com>
2025-04-19 15:37:19 +0530

    ji

--- Server/server.js
+++ Server/server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = 8000;
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: process.env.FRONTEND_URL, 
     credentials: true               // allow cookies
   }));
 
