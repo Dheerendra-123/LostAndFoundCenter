7a9c7f296d1aaa003666703aa8aea597705b3430
Dheerendra Vikram Dixit <dheerendradixit321@gmail.com>
2025-04-19 17:35:05 +0530

    hi

--- Server/server.js
+++ Server/server.js
@@ -15,6 +15,20 @@ app.use(cors({
      allowedHeaders: ['Content-Type', 'Authorization']        // allow cookies
   }));
 
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+      return res.status(200).end();
+    }
+    
+    next();
+  });
+
 app.use(express.json());
 require('dotenv').config();
 app.use(cookieParser());
